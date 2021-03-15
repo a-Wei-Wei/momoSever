0edcf6ac1be7bdcab57ed988f63eebf5bba2f2ee
lw <1043036235@qq.com>
2021-03-15 04:31:00 -0400

    修改获取方式

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import configparser
+import time
 
 config_parse = configparser.ConfigParser()
 config_parse.read("./momoUrl")
@@ -16,7 +17,32 @@ def getIps():
     iplist = re.findall(p,msg)
     return iplist
 
-# 这个函数就是 使用本地芝麻代理
+#暂时废弃，因为芝麻代理一次性不让获取大于20个的IP
+def getIps_form_zima():
+  url = config_parse.get("section three","url")
+  file = open("./ips.txt", "w")
+  res = requests.get(url)
+  soup = BeautifulSoup(res.text,"html.parser")
+  content = soup.string.replace("\r", "")
+  file.write(content)
+  time.sleep(2)
+  print("ips获取完毕, 共{0}".format(140))
+  # print(array_content)
+
+#代替上面的这个函数，反复获取6次。一共120个IP
+def getIps_from_zima_with_txt():
+  print("=====开始获取IP=====")
+  url = config_parse.get("section three", "url_20")
+  file = open("./ips.txt", "w")
+  for i in range(6):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text,"html.parser")
+    content = soup.string.replace("\r", "")
+    file.write(content)
+    time.sleep(2)
+  print("获取IP完毕，共计：{0}".format(6*20))
+
+# 这个函数就是 使用本地文件中获取芝麻代理ip
 def getIpsFromTxt():
     file = open("./ips.txt")
     msg = file.readlines()
@@ -24,30 +50,45 @@ def getIpsFromTxt():
     for temp in msg:
         temp = temp.replace("\r", "").replace("\n", "")
         list_temp.append(temp)
-
     return list_temp
 
 def main():
     # ipList = getIps()
+    # getIps_form_zima()
+    getIps_from_zima_with_txt()
     ipList = getIpsFromTxt()
+    print("开始运行访问服务，其中获取ip池中{0}个IP".format(len(ipList)))
     url = config_parse.get("section one", "url")
+    url_2 = config_parse.get("section tow", "url")
+    flag_url_2 = False
     for ip_temp in ipList:
         https_proxy = ip_temp
         proxies={
             'https': https_proxy
         }
         msg = ""
+        if ip_temp == "" or len(ip_temp) <= 0:
+          print("运行完毕")
+          continue
         try:
-            response = requests.get(url, proxies=proxies, timeout=5)
-            msg = response.text
+            if url_2 and flag_url_2:
+              response = requests.get(url_2, proxies=proxies, timeout=5)
+              msg = response.text
+              flag_url_2 = False
+            else:
+              response = requests.get(url, proxies=proxies, timeout=5)
+              msg = response.text
+              flag_url_2 = True
         except Exception as identifier:
             print("{0} timeOut".format(ip_temp))
         # print(msg)
         if "墨墨" in msg:
-            print("{0} , success".format(ip_temp))
+            print("{0} , success , {1}".format(ip_temp, flag_url_2))
         
 
 
 if __name__ == "__main__":
     main()
     # print(getIpsFromTxt())
+    # getIps_form_zima()
+    # getIps_from_zima_with_txt()
